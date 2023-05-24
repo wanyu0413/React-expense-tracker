6bfee84026cd699b5fa34fdb51accfd9b0e15d23
Wanyu Jiang <wanyu19930930@gmail.com>
2023-05-24 18:37:46 +0900

    form content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,27 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  <form></form>;
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <lable>Title</lable>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <lable>Amount</lable>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <lable>Date</lable>
+          <input type="date" min="2020-01-01" max="2023-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
