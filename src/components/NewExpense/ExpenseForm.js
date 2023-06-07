b770bfbaa4ab1d9ca6922904a4573c5304c1b8f6
Wanyu Jiang <wanyu19930930@gmail.com>
2023-06-07 20:44:52 +0900

    +

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,7 +30,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
